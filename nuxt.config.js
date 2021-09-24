acdd80ca6e481bcf3097753973366fb3aba55a43
Kode <kodestar@gmail.com>
2021-09-24 21:03:09 +0100

    update name

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,7 +63,7 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      name: 'Audioserve',
+      name: 'Audiosilo',
       lang: 'en',
       icons: [
         {
