da71bcf172712e2fb7b769660f0744f4d91c9cfa
Kode <kodestar@gmail.com>
2021-09-24 20:48:49 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'audioserve-ui',
+    title: 'audiosilo',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
