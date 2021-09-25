b1e0f8e87ce2676a0737e09c2001ee7cc0f895ad
Kode <kodestar@gmail.com>
2021-09-25 16:15:45 +0100

    fix search close button colour

--- components/Header.vue
+++ components/Header.vue
@@ -19,7 +19,7 @@
       <div :class="{ mobilesearch: search, flex: search }" class="px-0 lg:px-12 lg:w-full items-center flex items-center lg:border-l border-gray-100 dark:border-gray-750 h-14 lg:h-24">
         <floating-label-input :class="{ block: search, hidden: !search }" v-model="searchterm" title="Search..." />
         <div class="ml-3">
-          <div @click="search = false" :class="{ flex: search, hidden: !search }" class="rounded-full bg-gray-300 w-8 h-8 justify-center items-center"><i class="fa-thin fa-times fa-fw" /></div>
+          <div @click="search = false" :class="{ flex: search, hidden: !search }" class="rounded-full bg-gray-300 dark:bg-gray-860 w-8 h-8 justify-center items-center"><i class="fa-thin fa-times fa-fw" /></div>
         </div>
         <div :class="{ hidden: search }" class="flex lg:hidden justify-end text-lg mr-1">
           <div @click="search = true" class="bg-gray-200 dark:bg-gray-800 rounded w-10 h-10 flex justify-center items-center mx-1">
