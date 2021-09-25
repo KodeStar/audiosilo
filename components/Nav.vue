0f05c7ce35729e38e465a92adc9dba68df3c350e
Kode <kodestar@gmail.com>
2021-09-25 16:08:18 +0100

    some changes for mobile

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     :class="{ '': !menu, 'block': menu }"
-    class="transition-all z-40 bg-gray-200 dark:bg-gray-800 flex-shrink-0 flex-grow-0 w-screen transform lg:block lg:w-80 h-14 lg:h-full border-r border-gray-300 dark:border-gray-900 fixed bottom-0 lg:relative leftbar">
+    class="transition-all z-40 bg-gray-200 dark:bg-gray-800 flex-shrink-0 flex-grow-0 w-screen transform lg:block lg:w-80 lg:h-full border-r border-gray-300 dark:border-gray-900 fixed bottom-0 lg:relative leftbar pb-safe">
     <div v-if="collections.length > 1" class="hidden lg:block relative border-t border-gray-100 dark:border-gray-750 appearance-none flex-grow collection-container">
       <select :value="currentCollection" @change="changeCollection" class="w-full py-1 px-6 bg-gray-200 dark:bg-gray-800 text-sm leading-normal w-full">
         <option v-for="(option,index) in collections" :key="index" v-bind:value="index">
@@ -9,7 +9,7 @@
         </option>
       </select>
     </div>
-    <div class="lg:border-t border-gray-100 dark:border-gray-750 lg:p-8 px-2 lg:px-6 flex lg:flex-col justify-between lg:justify-start">
+    <div class="lg:border-t border-gray-100 dark:border-gray-750 lg:p-8 py-1 px-2 lg:px-6 flex lg:flex-col justify-between lg:justify-start">
       <NuxtLink :class="{ active: activepage === 'home' }" class="w-16 lg:w-full flex flex-col lg:flex-row items-center my-1 lg:my-2 p-1 lg:p-3 lg:px-5 rounded-lg nav" to="/">
         <i class="fa-light fa-fw fa-house lg:mr-2 fa-lg"></i>
         <span class="text-xs font-semibold lg:font-normal lg:text-base">Home</span>
